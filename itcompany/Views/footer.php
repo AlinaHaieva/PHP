8cac6e8dd37a7bc12711d05f4b42130069333f97
Alina Haieva <alina.gaevaya@gmail.com>
2017-07-28 16:08:58 +0300

    add tasks

--- itcompany/Views/footer.php
+++
@@ -1,7 +0,0 @@
-        </main>
-        <footer>
-            <hr>
-            <a class="btn" href="https://github.com/AlinaHaieva" target="_blank">Alina Haieva</a>
-        </footer>
-    <body>
-<html>
