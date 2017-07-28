6ee3017e149b8d6049ed77763780fb0bebe9ef03
Alina Haieva <alina.gaevaya@gmail.com>
2017-07-28 16:09:28 +0300

    add tasks

--- petshop/views/footer.php
+++
@@ -1,8 +0,0 @@
-            </div>
-        </main>
-        <footer>
-            <hr>
-            <a class="btn" href="https://github.com/AlinaHaieva" target="_blank">Alina Haieva</a>
-        </footer>
-    <body>
-<html>
